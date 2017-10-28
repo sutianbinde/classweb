fed4f8ec5052c1112331e38825f77b8b0bfbce01
sutianbinde <tianbinde163@163.com>
2017-10-28 20:25:07 +0800

    .

--- classweb/server/app.js
+++ classweb/server/app.js
@@ -13,7 +13,7 @@ var app = express();
 
 //跨域  后期删
 app.all('*', function(req, res, next) {
-		res.header("Access-Control-Allow-Origin", "http://localhost:8080"); //为了跨域保持session，所以指定地址，不能用*
+	res.header("Access-Control-Allow-Origin", "http://localhost:8080"); //为了跨域保持session，所以指定地址，不能用*
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
